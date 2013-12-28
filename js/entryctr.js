f73b1d5d8ef42d20b30e63083b3581d6970279b3
Alex Vallorosi <reddog9287@aim.com>
2013-12-28 17:36:34 -0500

    placeholder object

--- js/entryctr.js
+++ js/entryctr.js
@@ -2,7 +2,26 @@ var app = angular.module('EntryPage', []);
 
 app.controller("EntryController", function($scope, $http){
 
-    // $scope.editEntry = {};
+    $scope.editEntry = {
+        'image': 'http://www.placehold.it/300x300'
+        'title': 'Placeholder',
+        'subtitle': 'Placeholder',
+        'date': '01-01-2014',
+        'url': 'http://www.google.com',
+        'description': 'Placeholder',
+        'theme-color-1': 'red',
+        'theme-color-2': 'red',
+        'banner': {
+            'ad': 'http://www.google.com',
+            'image': 'http://www.placehold.it/600x100'
+        },
+        'x': '308290',
+        'y': '342809',
+        'button': {
+            'text': 'Submit',
+            'link': 'http://www.google.com'
+        }
+    };
 
     $scope.init = function() {
         console.log('Initialized');
