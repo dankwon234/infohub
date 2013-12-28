3178c8649ff5dfb2f6977c6b7512e2a8b3e2a332
Alex Vallorosi <reddog9287@aim.com>
2013-12-28 17:29:55 -0500

    comment out model

--- js/entryctr.js
+++ js/entryctr.js
@@ -2,7 +2,7 @@ var app = angular.module('EntryPage', []);
 
 app.controller("EntryController", function($scope, $http){
 
-	$scope.editEntry = {};
+    // $scope.editEntry = {};
 
     $scope.init = function() {
         console.log('Initialized');
