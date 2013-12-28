4a04d666bbe87e7cca5b783258c2f13a7482a376
Alex Vallorosi <reddog9287@aim.com>
2013-12-28 18:09:56 -0500

    fixed request parser

--- js/requestparser.js
+++ js/requestparser.js
@@ -1,7 +1,7 @@
 function parseLocation(stem){
 	console.log('PARSE LOCATION: '+stem);
 	var resourcePath = location.href.replace(window.location.origin, ''); // strip out the domain root (e.g. http://localhost:8888)
-	
+
 	var parts = resourcePath.split(stem+'/');
 	request = {"resource":null, "identifier":null};
 	if (parts.length > 1){
@@ -9,12 +9,13 @@ function parseLocation(stem){
 		for (var i=0; i<hierarchy.length; i++){
 			if (i==0)
 				request['resource'] = hierarchy[i]
-			
-			if (i==1)
-				request['identifier'] = hierarchy[i]
+
+			if (i==1) {
+			    request['identifier'] = hierarchy[i].split('?')[0];
+			}
 		}
 	}
-	
+
 	console.log(JSON.stringify(request));
 	return request;
 }
