c236e8331fa259fe47ee80a9032fddd6b6d36763
Alex Vallorosi <reddog9287@aim.com>
2013-12-28 17:21:42 -0500

    added requestParser file

---
+++ js/requestparser.js
@@ -0,0 +1,20 @@
+function parseLocation(stem){
+	console.log('PARSE LOCATION: '+stem);
+	var resourcePath = location.href.replace(window.location.origin, ''); // strip out the domain root (e.g. http://localhost:8888)
+	
+	var parts = resourcePath.split(stem+'/');
+	request = {"resource":null, "identifier":null};
+	if (parts.length > 1){
+		var hierarchy = parts[1].split('/');
+		for (var i=0; i<hierarchy.length; i++){
+			if (i==0)
+				request['resource'] = hierarchy[i]
+			
+			if (i==1)
+				request['identifier'] = hierarchy[i]
+		}
+	}
+	
+	console.log(JSON.stringify(request));
+	return request;
+}
