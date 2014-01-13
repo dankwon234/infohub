bf8e1e3f3507de1c876c8f1bf3ada16cc808dfed
Alex Vallorosi <reddog9287@aim.com>
2014-01-13 17:50:25 -0500

    whoops was on wrong version

--- js/entryctr.js
+++ js/entryctr.js
@@ -2,9 +2,7 @@ var app = angular.module('EntryPage', []);
 
 app.controller("EntryController", function($scope, $http){
 
-    $scope.editEntry = {
-        secondaryUrl: {}
-    };
+    $scope.editEntry = {};
 
     $scope.init = function() {
         console.log('Initialized');
