0bae55b788134f244c7ca731690bace06d01b9d8
Alex Vallorosi <reddog9287@aim.com>
2014-01-13 18:48:43 -0500

    remove some errors

--- js/entryctr.js
+++ js/entryctr.js
@@ -2,7 +2,9 @@ var app = angular.module('EntryPage', []);
 
 app.controller("EntryController", function($scope, $http){
 
-    $scope.editEntry = {};
+    $scope.editEntry = {
+        secondaryUrls: {}
+    };
 
     $scope.init = function() {
         console.log('Initialized');
