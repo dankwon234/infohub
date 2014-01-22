14880fd749ae8c5c37bbc9df5458acbdf0277f91
Alex Vallorosi <reddog9287@aim.com>
2014-01-22 18:23:30 -0500

    initial

--- js/device.js
+++ js/device.js
@@ -4,21 +4,21 @@
       var subcategorySelected = '';
       var errorMessages = new Array('Cant do that.', 'Seriously, thats not allowed.', 'stop it.');
       var errorCount = 0;
-      
-      
+
+
       function populateDeviceProfile(json){
 		  subcategorySelected = '';
     	  device = json;
 
     	  config = device["configuration"];
     	  categories = config['sequence'];
-    	  
+
     	  var categoriesHtml = '';
     	  for (var i=0; i<categories.length; i++){
     		  category = categories[i];
-    		  
+
     		  categoriesHtml += '<div class="media comment-item"><div id="'+category+'" draggable="true" ondragstart="dragCategory(event)" ondragenter="handleDragEnter(event)" ondragover="draggedOver(event)" ondragleave="dragLeave(event)" ondrop="dropCategory(event)" class="media-body"><h4 class="rs comment-author"><a style="color:#ea503d" onClick="return selectCategory(\''+category+'\');" href="#">'+category+'</a></h4>';
-    		  
+
     		  c = config[category];
     		  subs = c['order'];
     		  for (var k=0; k<subs.length; k++){
@@ -29,10 +29,10 @@
     		  categoriesHtml += '<br /><a style="color:red" onclick="return removeCategory('+i+');" href="#">remove category</a>';
     		  categoriesHtml += '</div></div>';
     	  }
-    	  
+
     	  document.getElementById('categories').innerHTML = categoriesHtml;
-    	  
-    	  
+
+
     	  radioHtml = '';
     	  if (device['isLive']=='yes'){
     		  radioHtml = '<input onchange="return radioChanged(this);" style="margin-right:5px" type="radio" name="isLive" value="yes" checked><label style="vertical-align:middle">YES</label><br />';
@@ -42,11 +42,11 @@
     		  radioHtml = '<input onchange="return radioChanged(this);" style="margin-right:5px" type="radio" name="isLive" value="yes"><label style="vertical-align:middle">YES</label><br />';
     		  radioHtml += '<input onchange="return radioChanged(this);" style="margin-right:5px" type="radio" name="isLive" value="no" checked><label style="vertical-align:middle">NO</label>';
     	  }
-    	  
+
     	  document.getElementById('radio').innerHTML = radioHtml;
       }
-    
-    
+
+
       function updateDevice(){
     	  device["uuid"] = document.getElementById('uuid').value;
     	  device["name"] = document.getElementById('name').value;
@@ -56,12 +56,12 @@
     	  device["loc3"] = document.getElementById('location3').value;
     	  device["loc4"] = document.getElementById('location4').value;
     	  device["image"] = document.getElementById('image').value;
-    	  
-    	  
+
+
     	  var xmlHttp = new XMLHttpRequest();
-    	  xmlHttp.open('PUT', '/api/device/'+device["uuid"], false); 
+    	  xmlHttp.open('PUT', '/api/device/'+device["uuid"], false);
     	  xmlHttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		
+
     	  var dataToSend = new FormData(); // create a new FormData object
     	  console.log('UPDATE DEVICE: '+JSON.stringify(device));
     	  dataToSend.append('device', JSON.stringify(device)); // add data to the object
@@ -74,46 +74,46 @@
     		  alert('UPDATE DEVICE - - SUCCESS: ');
     		  return false;
     	  }
-	    
+
     	  alert('UPDATE DEVICE - - FAIL: '+confirmation.results.message);
     	  return false;
       }
-      
-      
-      
+
+
+
       function selectCategory(category){
     	  selectedCategory = category;
     	  config = device["configuration"];
 		  c = config[category];
 		  console.log('SELECT CATEGORY: '+JSON.stringify(c));
     	  subcategoryKeys = c['order'];
-    	  
+
 		  subcategoriesHtml = '';
 		  for (var k=0; k<subcategoryKeys.length; k++){
 			  subcategoryName = subcategoryKeys[k];
 			  subcategoriesHtml += '<p style="margin-top:20px" class="rs fc-gray"><input id="'+category+'-'+k+'" class="subcategoryinput" type="text" value="'+subcategoryName+'"></p><div class="subcategory">';
-			
+
       		  providers = c[subcategoryName];
       		  for (var j=0; j<8; j++){
       			  provider = (j < providers.length) ? providers[j] : '';
       			  subcategoriesHtml += '<input id="'+subcategoryName+'-'+j+'" type="text" value="'+provider+'" /><br />';
       		  }
-				
+
       		  subcategoriesHtml += '</div>';
 		  }
-		  
+
 		  icon = c['icon'];
     	  document.getElementById('selectedcategory').innerHTML = '<h4 class="rs pb10">'+category+'</h4><hr /><div style="background-color:#999;border-radius:3px;text-align:center;padding:5px"><strong>Icon</strong><br /><a onclick="return showIcons();" href="#"><img src="/site/images/'+icon+'?crop=80" /></a><br />(click icon to change)</div>'+subcategoriesHtml+'<br /><div class="val"><button onClick="return commitCategory(\''+category+'\');" style="width:100%" class="btn btn-blue btn-submit-all">Commit</button></div>';
     	  return false;
       }
-      
-      
+
+
       function commitCategory(category){
     	  console.log('Commit Category: '+category);
-    	  
+
     	  config = device["configuration"];
 		  subcategories = config[category];
-		  
+
     	  subcategoryKeys = subcategories['order']; // ["sports","world","headlines","local","tech","business"]
     	  console.log('SUBCATEGORIES: '+JSON.stringify(subcategoryKeys));
 
@@ -128,20 +128,20 @@
       			  if (provider.length > 0)
       				  providers.push(provider);
       		  }
-      		  
+
 			  id = category+'-'+i;
 			  subcategory = document.getElementById(id).value;  // subcategory name might have been changed:
       		  updatedSubcategories[subcategory] = providers;
       		  updatedOrder.push(subcategory);
 		  }
-		  
+
     	  updatedSubcategories['order'] = updatedOrder; // update the order
     	  updatedSubcategories['icon'] = subcategories['icon']; // update the icon
 
     	  console.log('UPDATED SUBCATEGORIES: '+JSON.stringify(updatedSubcategories));
     	  config[category] = updatedSubcategories;
     	  device["configuration"] = config;
-    	  
+
     	  updateDevice();
     	  return false;
       }
@@ -150,14 +150,14 @@
     	  venues = document.getElementById('venues');
     	  newVenueId = venues.options[venues.selectedIndex].value;
     	  console.log('Venue Selected: ');
-    	  
+
     	  document.getElementById('venue id').value = newVenueId;
       }
-      
-      
+
+
       function deleteDevice(){
     	  confirmation = confirm('Are You Sure?');
-    	  
+
     	  if (confirmation){
   	  			var xmlHttp = new XMLHttpRequest();
 				xmlHttp.open('DELETE', '/api/device/'+device["uuid"], false);
@@ -177,8 +177,8 @@
     	  }
     	  return false;
       }
-      
-      
+
+
 	    function processTime(time){
 	    	date = moment(new Date(time)).format('MMM D, h:mma');
 	    	return date;
@@ -192,8 +192,8 @@
     	  return obj;
       }
 
-      
-    
+
+
       function fetchSessions(){
     	var url = '/api/sessions?device='+device['uuid'];
     	response = executeUrlRequest(url, 'GET');
@@ -201,7 +201,7 @@
     	if (results.confirmation=='success'){
     		sessions = results.sessions;
 //    		console.log('FETCH SESSIONS: '+JSON.stringify(sessions));
-    		
+
     		list = '<ol>';
     		for (var i=0; i<sessions.length; i++){
     			session = sessions[i];
@@ -209,12 +209,12 @@
     			list += '<li><a target="_blank" href="/site/sessions/'+session['id']+'">'+processTime(date)+'</a></li>'
     		}
     		list += '</ol>';
-    		
+
       	    document.getElementById('sessions').innerHTML = list;
       	    fetchCateogories();
     		return;
     	}
-    	
+
     	alert(results['message']);
     	return;
     }
@@ -227,77 +227,77 @@
 
       function draggedOver(event){
     	  event.preventDefault();
-    	  
+
 //    	  console.log('DRAGGED OVER: '+event.target.parentNode.id);
     	  event.dataTransfer.dropEffect = 'move';
       }
-      
+
       function dragLeave(event){
 //    	  console.log('DRAG LEAVE: '+event.target.parentNode.id);
     	  event.target.classList.remove('over');
 
       }
-      
+
       function dragCategory(event){
     	  console.log('DRAG CATEGORY: '+event.target.className);
     	  event.dataTransfer.setData("categoryId", event.target.id);
       }
-      
+
       function dropCategory(event){
     	  console.log('DROP CATEGORY: '+event.target.id);
     	  var movedCategory = event.dataTransfer.getData("categoryId");
     	  console.log('MOVED CATEGORY: '+movedCategory);
-    	  
+
     	  if (movedCategory==null){
         	  populateDeviceProfile(device);
         	  event.preventDefault();
         	  return;
     	  }
-    	  
+
     	  if (movedCategory.length < 2){
         	  populateDeviceProfile(device);
         	  event.preventDefault();
         	  return;
     	  }
-    	  
+
     	  var replacedCategory = event.target.parentNode.id;
     	  console.log('REPLACED CATEGORY: '+replacedCategory);
-    	  
+
     	  if (movedCategory==replacedCategory){ // no change, ignore
         	  populateDeviceProfile(device);
         	  event.preventDefault();
         	  return;
     	  }
-    	  
+
     	  config = device['configuration'];
     	  sequence = config['sequence'];
     	  console.log('SEQUENCE 1: '+JSON.stringify(sequence));
-    	  
+
     	  var index = sequence.indexOf(movedCategory);
     	  sequence.splice(index, 1);
     	  console.log('SEQUENCE 2: '+JSON.stringify(sequence));
-    	  
+
     	  updatedSequence = new Array();
     	  for (var i=0; i<sequence.length; i++){
     		  category = sequence[i];
     		  console.log('INDEX '+i+" == "+category);
-    		  
+
     		  if (category==replacedCategory){
     			  updatedSequence.push(movedCategory);
     		  }
-    		  
+
     		  updatedSequence.push(category);
     	  }
     	  console.log('SEQUENCE 3: '+JSON.stringify(updatedSequence));
-    	  
-    	  
+
+
     	  config['sequence'] = updatedSequence;
     	  device['configuration'] = config;
 
     	  populateDeviceProfile(device);
     	  event.preventDefault();
       }
-      
+
       function subcategoryTapped(buttonId){
     	  console.log('subcategoryTapped '+buttonId);
     	  button = document.getElementById(buttonId);
@@ -318,23 +318,23 @@
     	  parts = subcategorySelected.split("-");
     	  categoryName = parts[0];
     	  subcategoryName = parts[1];
-    	  
+
     	  p = buttonId.split("-");
     	  movingCategory = p[0];
     	  movingSubcategory = p[1];
 
-    	  
+
     	  if (categoryName!=movingCategory){
     		  i = errorCount % errorMessages.length;
     		  alert(errorMessages[i]);
     		  errorCount++;
     		  return false;
     	  }
-    	  
+
 
     	  config = device['configuration'];
     	  category = config[categoryName];
-    	  
+
     	  updatedOrder = new Array();
     	  order = category['order'];
     	  for (var i=0; i<order.length; i++){
@@ -347,49 +347,49 @@
         	  else
         		  updatedOrder.push(sub);
     	  }
-    	  
+
     	  category['order'] = updatedOrder;
     	  config[categoryName] = category;
     	  device['configuration'] = config;
     	  populateDeviceProfile(device);
-    	  
+
     	  selectCategory(categoryName);
     	  return false;
       }
-      
-      
+
+
       function addClass(el, className){
     	    el.className += ' '+className;
       }
-      
+
       function removeClass(el, classNm){
     	  var elClass = ' '+el.className+' ';
     	  while(elClass.indexOf(' '+classNm+' ') != -1)
     		  elClass = elClass.replace(' '+classNm+' ', '');
-    	  
+
     	  el.className = elClass;
       }
-      
+
       function showSection(section){
     	  console.log('Show Section: '+section);
     	  sidebar = document.getElementById('sidebar');
     	  if (section=='device'){
     		  deviceTemplate = $('#deviceinfo').html();
-    		  
+
     		  // populate with specific device information
     		  deviceTemplate = deviceTemplate.replace("{{deviceId}}", device['uuid']);
     		  deviceTemplate = deviceTemplate.replace("{{timestamp}}", device['timestamp']);
     		  deviceTemplate = deviceTemplate.replace("{{image}}", device['image']);
-    		  
+
     		  sidebar.innerHTML = deviceTemplate;
     		  return true;
     	  }
-    	  
+
 
     	  if (section=='configuration'){
     		  configurationTemplate = $('#configuration').html();
-    		  
-    		  
+
+
     		  sidebar.innerHTML = configurationTemplate;
     		  return true;
     	  }
@@ -399,22 +399,22 @@
     		  sessionsTemplate = $('#sessionInfo').html();
     		  sessionsTemplate = sessionsTemplate.replace("{{numSessions}}", device['numSessions']);
     		  sessionsTemplate = sessionsTemplate.replace("{{totalSeconds}}", device['totalSeconds']);
-    		  
+
     		  sidebar.innerHTML = sessionsTemplate;
     		  return true;
     	  }
-    	  
+
 
     	  if (section=='providers'){
     		  configuration = device['configuration'];
     		  sequence = configuration.sequence;
-    		  
+
     		  content = '';
     		  totalProviders = 0;
     		  for (var i=0; i<sequence.length; i++){
     			  categoryName = sequence[i];
     			  content += '<div style="padding:10px;border-bottom:1px dotted #888"><h3>'+categoryName+'</h3>';
-    			  
+
         		  console.log(JSON.stringify(categoryName));
 
         		  category = configuration[categoryName];
@@ -423,7 +423,7 @@
         			  subcategory = order[j];
         			  content += '<h4>'+subcategory+'</h4>';
         			  providers = category[subcategory];
-        			  
+
             		  console.log(subcategory+': '+JSON.stringify(providers));
             		  if (providers.length > 0){
             			  content += '<ol style="padding:0px 0px 0px 20px">';
@@ -433,7 +433,7 @@
             				  url = providers[k];
             				  if (provider.length > 60)
             					  provider = provider.substring(0, 60)+'...';
-            					  
+
             				  content += '<li><a style="color:#ea503d;" target="_blank" href="'+url+'">'+provider+'</a></li>';
             			  }
             			  content += '</ol>';
@@ -441,40 +441,40 @@
             		  else{
             			  content += 'none';
             		  }
-        			  
-        			  
+
+
         		  }
-    			  
+
     			  content += '</div>';
     		  }
-    		  
+
     		  document.getElementById('providers').innerHTML = content;
-    		  
+
     		  providersSidebar = $('#providersSidebar').html();
     		  providersSidebar = providersSidebar.replace("{{numProviders}}", totalProviders);
     		  providersSidebar = providersSidebar.replace("{{deviceId}}", device['uuid']);
-    		  
-    		  
+
+
     		  sidebar.innerHTML = providersSidebar;
-    		  
+
     		  return true;
     	  }
 
-    	  
-    	  
+
+
     	  return true;
       }
-      
+
       function radioChanged(radio){
     	  console.log('RADIO CHANGED: '+radio.value);
     	  device["isLive"] = radio.value;
       }
-      
+
       function addCategory(){
     	  categoryId = document.getElementById('addcategory').value;
     	  console.log('ADD CATEGORY: '+categoryId);
     	  url = '/api/device/'+device['uuid']+'?action=addcategory&category='+categoryId;
-    	  
+
     	  response = executeUrlRequest(url, 'PUT');
     	  results = response['results'];
     	  confirmation = results['confirmation'];
@@ -489,7 +489,7 @@
 		  alert(results['message']);
 		  return;
       }
-      
+
       function fetchCateogories(){
     	  url = '/api/categories';
     	  response = executeUrlRequest(url, 'GET');
@@ -497,24 +497,24 @@
     	  confirmation = results['confirmation'];
     	  if (confirmation=='success'){
     		  categories = results['categories'];
-    		  
+
     		  list = '';
     		  for (var i=0; i<categories.length; i++){
     			  category = categories[i];
     			  list += '<option value="'+category['id']+'">'+category['name']+'</option>';
     		  }
-    		  
+
     		  document.getElementById('addcategory').innerHTML = list;
     	  }
       }
-      
+
       function removeCategory(index){
     	  config = device["configuration"];
     	  categoriesSequence = config['sequence'];
 		  categoryName = categoriesSequence[index];
-		  
+
     	  url = '/api/device/'+device['uuid']+'?action=removecategory&category='+categoryName;
-    	  
+
     	  response = executeUrlRequest(url, 'PUT');
     	  results = response['results'];
     	  confirmation = results['confirmation'];
@@ -528,15 +528,34 @@
 		  alert(results['message']);
     	  return false;
       }
-      
+
       function showIcons(){
     	  console.log('Show Icons');
-    	  
-    	  
+
+
     	  popup('/site/images');
     	  return false;
       }
-      
+
+      function selectEntry(entryId) {
+          console.log("ENTRY ID: "+entryId);
+
+          config = device["configuration"];
+          c = config[selectedCategory];
+
+          // Clicking on input field triggers the popup to show up
+          // -----Magic happens in controller---------------------
+          // Controller spits out an id
+          // Take that id (entryId) here in this function
+          // and set it equal to the index in the array of the json
+          // that initially triggered the popup (ie. "Google maps"[0])
+
+          // c[''] =
+
+
+		  return;
+      }
+
       function selectIcon(iconId){
     	  console.log('Select Icon: '+iconId);
 
@@ -544,13 +563,13 @@
 		  c = config[selectedCategory];
 		  c['icon'] = iconId;
     	  console.log('Category: '+JSON.stringify(c));
-		  
+
 		  config[selectedCategory] = c;
 		  device["configuration"] = config;
 		  selectCategory(selectedCategory);
 		  return;
       }
-      
+
 	    function popup(url) {
 		  	newwindow = window.open(url,'name','height=450,width=900');
 		  	if (window.focus) {
@@ -558,5 +577,5 @@
 		  	}
 		  	return false;
 		  }
-      
-      
+
+
---
+++ js/selectentriesctr.js
@@ -0,0 +1,45 @@
+var app = angular.module('SelectEntriesPopup', []);
+
+app.controller("SelectEntriesController", function($scope, $http){
+    // configuration holds ids which correspond to /api/entries/:id
+    $scope.init = function() {
+        fetchEntries();
+    }
+
+    function fetchEntries () {
+        var url = '/api/entries';
+        $http.get(url)
+        .success(function(data, status, headers, config) {
+            results = data['results'];
+            confirmation = results['confirmation'];
+            if (confirmation=='success'){
+                $scope.entries = results['entries'];
+            } else {
+                alert(results['message']);
+            }
+        }).error(function(data, status, headers, config) {
+            console.log("error", data, status, headers, config);
+        });
+    }
+
+	$scope.select = function(index){
+        parent = window.opener;
+        if (!parent)
+            return true;
+
+        parent.selectEntry($scope.entries[index].id); // pass back the uniqueId of the entry
+
+        window.close();
+        return false;
+  	}
+
+      //     /site/selectentries = url
+      //
+      //     $scope.popup = function(url) {
+      //     newwindow = window.open(url,'name','height=450,width=900');
+      //     if (window.focus) {
+      //         newwindow.focus();
+      //     }
+      //     return false;
+      // }
+});
