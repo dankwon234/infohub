20332adfd315a00a14bdf15645b38e22ea7beca2
Alex Vallorosi <reddog9287@aim.com>
2014-02-18 14:55:12 -0500

    added angular

---
+++ js/devicectr.js
@@ -0,0 +1,24 @@
+var app = angular.module('Device', []);
+
+app.controller("RecordsController", function($scope, $http){
+
+    $scope.init = function() {
+        fetchRecords();
+    }
+
+    function fetchRecords () {
+        var url = '/api/records';
+        $http.get(url)
+        .success(function(data, status, headers, config) {
+            results = data['results'];
+            confirmation = results['confirmation'];
+            if (confirmation=='success'){
+                $scope.records = results['records'];
+            } else {
+                alert(results['message']);
+            }
+        }).error(function(data, status, headers, config) {
+            console.log("error", data, status, headers, config);
+        });
+    }
+});
