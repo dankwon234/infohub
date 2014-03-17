7c5984ab62f027aa217e2cb165dbaa86ddbf340a
Alex Vallorosi <reddog9287@aim.com>
2014-03-16 21:06:07 -0400

    logging

--- js/devicectr.js
+++ js/devicectr.js
@@ -170,7 +170,7 @@ app.controller("ConfigController", function($scope, $http, sidebar, popup){
             console.log('Show Entries');
             // sidebar.currentCategory = $scope.device.configuration[category][subcategory][entry];
             sidebar.currentEntry = entry;
-            console.log(sidebar.currentCategory[subcategory][entry]);
+            console.log(sidebar.currentCategory[subcategory]);
             // sidebar.currentCategory = $scope.device.configuration[current];
 
             // popup.entry = $scope.device.configuration[category][subcategory][entry];
