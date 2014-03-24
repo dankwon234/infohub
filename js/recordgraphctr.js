d8631b8d031a06c4d5d80752d591f345ea825f84
Alex Vallorosi <reddog9287@aim.com>
2014-03-24 19:59:30 -0400

    cleaning out code and logging dateMap values

--- js/recordgraphctr.js
+++ js/recordgraphctr.js
@@ -1,20 +1,9 @@
 var app = angular.module('RecordsGraph', []);
 
 app.controller('RecordsGraphController', function($scope, $http) {
-    // $scope.testData = [{
-    //                 name: "test", // Device Name/ID,
-    //                 data: [10, 10, 20, 5, 1, 2, 3,10, 10, 20, 5, 1, 2, 3,10, 10, 20, 5, 1, 2, 3,10, 10, 20, 5, 1, 2, 3,10, 10, 20, 5, 1, 2, 3] // array of # of records (obviously each item in the array is records per day),
-    //                                                // data: scope.items
-    //             },{
-    //                 name: "test2", // Device Name/ID,
-    //                 data: [10, 10, 20, 5, 1, 2, 3,11, 10, 20, 5, 1, 2, -5,10, 10, 20, 5, 1, 2, 17,10, 10, 20, 5, 1, 2, 9,10, 10, 10, 5, 1, 2, 3] // array of # of records (obviously each item in the array is records per day),
-    //                                                // data: scope.items
-    //             }];
-
     $scope.dates = [];
     $scope.init = function() {
         fetchDevices();
-        // fetchRecords('77654979-CCDD-499F-AF90-CC23C60879D8');
     }
 
     $scope.fetchRecords = function(id, name) {
@@ -32,73 +21,29 @@ app.controller('RecordsGraphController', function($scope, $http) {
             if (confirmation=='success'){
             	$scope.records = results['records'];
                 var data = [];
-                var prevDate = "";
-                var j=0;
                 var dateMap = {};
                 for (var i=0;i<$scope.records.length;i++) {
-
-                    // if
-                    // current date: $scope.records[i].date.slice(0,10)
                     var curDate = $scope.records[i].date.slice(0,10);
 
                     if (dateMap[curDate]) {
                         dateMap[curDate]++;
-                        console.log('exists');
-                        console.log(dateMap);
                     } else {
                         dateMap[curDate] = 1;
-                        console.log('new');
-                        console.log(dateMap);
                     }
-
-                    // console.log("LAST DATE: ");
-                    // console.log(prevDate);
-                    // console.log("CURRENT RECORD'S DATE: ");
-                    // console.log(curDate);
-                    //
-                    // j++;
-                    // if (curDate == prevDate) {
-                    //     console.log("curDate == prevDate, COUNT: "+j);
-                    // } else {
-                    //     console.log("prevDate = curDate");
-                    //     prevDate = curDate;
-                    //     data.push(j);
-                    //     j=1;
-                    // }
-
-
-
-
-
-                    // dateArray.push($scope.records[i].date.slice(0,10));
-
-                    // // on a record. so increment
-                    // j++;
-                    // // if the record's date is not equal to last date,
-
-                    // if ($scope.records[i].date.slice(0,10) != lastDate) {
-                    //     //push date
-                    //     lastDate = $scope.records[i].date.slice(0,10);
-                    //     data.push(j);
-                    //     j=0;
-                    // } else {
-                    //     j++;
-                    // }
-
-                    // data.push(number of records on that date);
-                    // j++;
-                    // Array of objects with name: deviceID, data: [# of records per each day]
-
-                    // if (dateMap.curDate)
-                    // dateMap[curDate]
 
                     if ($scope.dates.indexOf(curDate) == -1) {
                         $scope.dates.push(curDate);
                     }
                 }
+
+                var keys = Object.keys(dateMap);
+                for (var i=0;i<keys.length;i++) {
+                    console.log(dateMap.keys[i]);
+                }
+
                 console.log("DATA: ");
                 console.log(data);
-                // console.log($scope.dates);
+
                 // $scope.series.push({
                 //     name: name,
                 //     data: data
@@ -118,7 +63,6 @@ app.controller('RecordsGraphController', function($scope, $http) {
             confirmation = results['confirmation'];
             if (confirmation=='success'){
             	$scope.devices = results['devices'];
-                console.log('success');
             } else {
                 alert(results['message']);
             }
