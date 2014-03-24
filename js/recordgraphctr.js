f56e427ba021fbf1e0cf1e47a6815411f75b4b4d
Alex Vallorosi <reddog9287@aim.com>
2014-03-24 17:39:06 -0400

    setting blank dates array at top of controller

--- js/recordgraphctr.js
+++ js/recordgraphctr.js
@@ -1,6 +1,8 @@
 var app = angular.module('RecordsGraph', []);
 
 app.controller('RecordsGraphController', function($scope, $http) {
+    $scope.dates = [];
+
     $scope.init = function() {
     	fetchRecords('77654979-CCDD-499F-AF90-CC23C60879D8');
     }
