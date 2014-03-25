325f2c391140574386346b6856beeb6f6ded96f3
Alex Vallorosi <reddog9287@aim.com>
2014-03-25 17:28:55 -0400

    add a ref to an empty object on top

--- js/recordgraphctr.js
+++ js/recordgraphctr.js
@@ -1,6 +1,7 @@
 var app = angular.module('RecordsGraph', []);
 
 app.controller('RecordsGraphController', function($scope, $http) {
+    $scope.series = {};
     $scope.seriesCache = {};
     $scope.dates = [];
 
@@ -9,6 +10,7 @@ app.controller('RecordsGraphController', function($scope, $http) {
     }
 
     $scope.fetchRecords = function(deviceId, deviceName) {
+        // @NOTE: series is cached -- retrieve from cache
         if ($scope.seriesCache[deviceId]) {
             console.log('series cache');
             var curSeries = $scope.seriesCache[deviceId];
@@ -17,49 +19,51 @@ app.controller('RecordsGraphController', function($scope, $http) {
                 name: curSeries.name,
                 data: curSeries.data
             };
-        } else {
-            console.log("new device. API Call");
-            var url = '/api/records?device=' + deviceId;
-            $http.get(url).success(function(data, status, headers, config) {
-                results = data['results'];
-                confirmation = results['confirmation'];
-                if (confirmation=='success'){
-                	$scope.records = results['records'];
-                    var data = [];
-                    var dateMap = {};
-                    for (var i=0;i<$scope.records.length;i++) {
-                        var curDate = $scope.records[i].date.slice(0,10);
-
-                        if (dateMap[curDate]) {
-                            dateMap[curDate]++;
-                        } else {
-                            dateMap[curDate] = 1;
-                        }
-
-                        if ($scope.dates.indexOf(curDate) == -1) {
-                            $scope.dates.unshift(curDate);
-                        }
-                    }
-
-                    var keys = Object.keys(dateMap);
-                    for (var i=0;i<keys.length;i++) {
-                        data.push(dateMap[keys[i]]);
-                    }
-
-                    $scope.series = {
-                        id: deviceName,
-                        name: deviceName,
-                        data: data
-                    };
-
-                    $scope.seriesCache[deviceId] = $scope.series;
-                } else {
-                    alert(results['message']);
-                }
-            }).error(function(data, status, headers, config) {
-                console.log("error", data, status, headers, config);
-            });
+            return;
         }
+
+        // @NOTE: series is not cached -- API Request
+        console.log("new device. API Call");
+        var url = '/api/records?device=' + deviceId;
+        $http.get(url).success(function(data, status, headers, config) {
+            results = data['results'];
+            confirmation = results['confirmation'];
+            if (confirmation=='success'){
+            	$scope.records = results['records'];
+                var data = [];
+                var dateMap = {};
+                for (var i=0;i<$scope.records.length;i++) {
+                    var curDate = $scope.records[i].date.slice(0,10);
+
+                    if (dateMap[curDate]) {
+                        dateMap[curDate]++;
+                    } else {
+                        dateMap[curDate] = 1;
+                    }
+
+                    if ($scope.dates.indexOf(curDate) == -1) {
+                        $scope.dates.unshift(curDate);
+                    }
+                }
+
+                var keys = Object.keys(dateMap);
+                for (var i=0;i<keys.length;i++) {
+                    data.push(dateMap[keys[i]]);
+                }
+
+                $scope.series = {
+                    id: deviceName,
+                    name: deviceName,
+                    data: data
+                };
+
+                $scope.seriesCache[deviceId] = $scope.series;
+            } else {
+                alert(results['message']);
+            }
+        }).error(function(data, status, headers, config) {
+            console.log("error", data, status, headers, config);
+        });
     }
 
     function fetchDevices() {
