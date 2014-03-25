8339bd47f52e653ead72c3c00d9710ca7771eabd
Alex Vallorosi <reddog9287@aim.com>
2014-03-25 18:14:38 -0400

    keeping consistent names

--- js/recordgraphctr.js
+++ js/recordgraphctr.js
@@ -83,7 +83,7 @@ app.controller('RecordsGraphController', function($scope, $http) {
     }
 });
 
-app.directive('hcPie', function () {
+app.directive('hc-line', function () {
     return {
         restrict: 'C',
         replace: true,
